c614967e183c3bc0ebf420157aa986316be79fee
Ariel Elkin <ariel@shapedbyiris.com>
2021-06-07 10:05:10 +0100

    add tests

---
+++ CIAudioProblemReproUITests/CIAudioProblemReproUITests.swift
@@ -0,0 +1,21 @@
+//
+//  CIAudioProblemReproUITests.swift
+//  CIAudioProblemReproUITests
+//
+//  Created by Ariel Elkin on 07/06/2021.
+//
+
+import XCTest
+
+class CIAudioProblemReproUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+    }
+}
