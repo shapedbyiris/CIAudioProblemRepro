267445e7036fd017d6ba4ef21f55671944a30bc0
Ariel Elkin <ariel@shapedbyiris.com>
2021-06-07 10:00:43 +0100

    first commit

---
+++ CIAudioProblemRepro/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  CIAudioProblemRepro
+//
+//  Created by Ariel Elkin on 07/06/2021.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    let engine = AVAudioEngine()
+    let player = AVAudioPlayerNode()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let url = Bundle.main.url(forResource: "audiofile", withExtension: "wav")!
+
+        let file = try! AVAudioFile(forReading: url)
+
+        let buffer = AVAudioPCMBuffer(pcmFormat: file.processingFormat, frameCapacity: UInt32(file.length))!
+        try! file.read(into: buffer)
+
+        let effectNode = AVAudioUnitReverb()
+
+        engine.attach(player)
+        engine.attach(effectNode)
+
+        engine.connect(player, to: effectNode, format: file.processingFormat)
+        engine.connect(effectNode, to: engine.mainMixerNode, format: file.processingFormat)
+
+//        player.scheduleBuffer(buffer, at: nil, options: .loops, completionHandler: nil)
+
+        try! engine.start()
+        //        player.play()
+        //
+        //        playbackUpdateSubject.send()
+
+        print("Engine started successfully")
+    }
+}
